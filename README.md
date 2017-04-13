0a98641eb52c5108a2a76f05ec924d09dfb27ece
Patrik Nyman <patrik911217@gmail.com>
2017-04-13 15:06:13 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Connect to database
+
+Create ping route
+
+Save to database
+
+Create login route
+
+Create angular 2 client
---
+++ server.js
@@ -0,0 +1,29 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+exports.app = app;
