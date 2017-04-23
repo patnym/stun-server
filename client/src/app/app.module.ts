e3ecb943bc319dc3b65710fe48d37a669562cf13
Patrik Nyman <patrik911217@gmail.com>
2017-04-23 13:33:14 +0200

    Routing init, Dashboard page init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
 import { AuthService } from './services/authentication/auth.service';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }, //login page
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)    
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
