a52512b252898245abcde06fdefa4a12f5300b31
Patrik Nyman <patrik911217@gmail.com>
2017-04-23 15:30:49 +0200

    verifies the user is logged in on protected routes

--- client/src/app/services/authentication/auth-guard.service.ts
+++ client/src/app/services/authentication/auth-guard.service.ts
@@ -1,10 +1,15 @@
 import { Injectable }     from '@angular/core';
 import { CanActivate }    from '@angular/router';
+import { AuthService }    from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService) {
+    
+  }
+
   canActivate() {
-    console.log('AuthGuard#canActivate called');
-    return true;
+    return this.auth.isAuthenticated();
   }
 }
