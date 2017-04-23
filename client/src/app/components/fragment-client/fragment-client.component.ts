65da1c34a4b4c11d81363c68f1cf8a6d34704fec
Patrik Nyman <patrik911217@gmail.com>
2017-04-23 18:39:35 +0200

    added client model field

--- client/src/app/components/fragment-client/fragment-client.component.ts
+++ client/src/app/components/fragment-client/fragment-client.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientModel } from '../../models/client.model';
 
 @Component({
   selector: 'app-fragment-client',
-  templateUrl: './fragment-client.component.html',
   inputs: ['model'],
+  templateUrl: './fragment-client.component.html',
   styleUrls: ['./fragment-client.component.css']
 })
 export class FragmentClientComponent implements OnInit {
+
+  model: ClientModel;
 
   isCollapsed: boolean = false;
 
